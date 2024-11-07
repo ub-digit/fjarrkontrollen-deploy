138e832671feb2528e5c4380076cc7525f172c5c
David Gustafsson <david.gustafsson@ub.gu.se>
2024-11-07 14:12:50 +0100

    Fix database filename

--- init-dev.sh
+++ init-dev.sh
@@ -19,7 +19,7 @@ fi
 mkdir -p db-data
 mkdir -p forms-db-data
 
-ssh apps@$SSH_HOST "cd $REPO_DIR/fjarrkontrollen-deploy && ./docker-compose-release-mailpit.sh exec db pg_dump --insert -O -x -U fjarrkontrollen_user fjarrkontrollen_db" > ./postgres-initdb.d/database,sql
+ssh apps@$SSH_HOST "cd $REPO_DIR/fjarrkontrollen-deploy && ./docker-compose-release-mailpit.sh exec db pg_dump --insert -O -x -U fjarrkontrollen_user fjarrkontrollen_db" > ./postgres-initdb.d/database.sql
 echo "Backend datababase immported from lab"
 
 ssh apps@$SSH_HOST "cd $REPO_DIR/fjarrkontrollen-deploy && ./docker-compose-release-mailpit.sh exec forms_db pg_dump --insert -O -x -U fjarrkontrollen_forms_user fjarrkontrollen_forms_db" > ./forms-postgres-initdb.d/database,sql
